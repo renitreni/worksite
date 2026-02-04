97b7d23f7fff978eaa55e0575d5d02ad07c0b09d
ian <dznn20@gmail.com>
2026-02-04 14:11:06 +0800

    Combine the route in the web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('main');
@@ -14,8 +15,68 @@ Route::get('/register', function () {
     return view('auth.register');
 })->name('register');
 
+#CANDIDATE ROUTES
+
+Route::get('/candidate', function(){
+    return view('candidate.layout');
+})->name('candidate');
+
+Route::get('/candidate/dashboard', function(){
+    return view('candidate.contents.dashboard');
+})->name('candidate.dashboard');
+
+Route::get('/candidate/profile', function () {
+    return view('candidate.contents.profile');
+})->name('candidate.profile');
+
+Route::get('/candidate/my-resume', function () {
+    return view('candidate.contents.my-resume');
+})->name('candidate.my-resume');
+
+Route::get('/candidate/my-applied-jobs', function () {
+    return view('candidate.contents.my-applied-jobs');
+})->name('candidate.my-applied-jobs');
+
+Route::get('/candidate/shortlist-jobs', function () {
+    return view('candidate.contents.shortlist-jobs');
+})->name('candidate.shortlist-jobs');
+
+Route::get('/candidate/following-employers', function () {
+    return view('candidate.contents.following-employers');
+})->name('candidate.following-employers');
+
+Route::get('/candidate/job-alerts', function () {
+    return view('candidate.contents.job-alerts');
+})->name('candidate.job-alerts');
+
+Route::get('/candidate/messages', function () {
+    return view('candidate.contents.messages');
+})->name('candidate.messages');
+
+Route::get('/candidate/meetings', function () {
+    return view('candidate.contents.meetings');
+})->name('candidate.meetings');
+
+Route::get('/candidate/change-password', function () {
+    return view('candidate.contents.change-password');
+})->name('candidate.change-password');
+
+Route::get('/candidate/delete-profile', function () {
+    return view('candidate.contents.delete-profile');
+})->name('candidate.delete-profile');
 
 
+Route::post('/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect('/login');
+})->name('logout');
+
+
+
+#ADMIN ROUTESSS
 Route::view('/admin/adminlogin', 'adminpage.contents.adminlogin')->name('admin.adminlogin');
 
 Route::view('/admin', 'adminpage.contents.dashboard')->name('admin.dashboard');
